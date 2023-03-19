b9e4d88bf9c9a312d6bff31144db4caccced5e98
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-19 20:06:15 +0200

    fix class if kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -15,11 +15,24 @@ class BaseModel:
             self.updated_at = datetime.now()
             storage.new(self)
         else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+            if kwargs.get("updated_at", None) and type(kwargs['updated_at']) is str:
+                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+            else:
+                self.updated_at = datetime.utcnow()
+
+            if kwargs.get("created_at", None) and type(kwargs['created_at']) is str:
+                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            else:
+                self.created_at = datetime.utcnow()
+            
+            if kwargs.get("__class__", None):
+                del kwargs['__class__']
+
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
+
             self.__dict__.update(kwargs)
 
     def __str__(self):
