b2fe7823d08d22796470f17f85921a9e22c45a62
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-20 12:34:53 +0200

    implement DBStorage on city & place & user

--- models/city.py
+++ models/city.py
@@ -3,15 +3,17 @@
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, Integer, String, ForeignKey
-from os import getenv
+import models
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
+    if models.s_type == 'db':
         __tablename__ = "cities"
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+        places = relationship("Place", backref="cities", cascade="delete")
     else:
         name = ""
         state_id = ""
--- models/user.py
+++ models/user.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel, Base
 import models
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -13,6 +14,7 @@ class User(BaseModel, Base):
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
         last_name = Column(String(128), nullable=False)
+        places = relationship('Place', backref="user", cascade="delete")
     else:
         email = ''
         password = ''
