7c67fbe01154aadd7a645eec6b588f11bc0a29dd
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-20 12:07:09 +0200

    change _tablename__ to __tablename__

--- models/user.py
+++ models/user.py
@@ -8,7 +8,7 @@ from sqlalchemy import Column, Integer, String
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     if models.s_type == "db":
-        _tablename__ = "users"
+        __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=False)
