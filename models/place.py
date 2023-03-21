9992c72186a4340ed1acabe00884b45048fceab3
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-03-21 13:46:59 +0200

    add relations between amenties and places

--- models/place.py
+++ models/place.py
@@ -2,8 +2,18 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 import models
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
+
+if models.s_type == 'db':
+    place_amenity = Table("place_amenity", Base.metadata,
+                          Column("place_id", String(60),
+                                 ForeignKey("places.id"),
+                                 primary_key=True, nullable=False),
+                          Column("amenity_id", String(60),
+                                 ForeignKey("amenities.id"),
+                                 primary_key=True, nullable=False))
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -20,6 +30,8 @@ class Place(BaseModel, Base):
         latitude = Column(Float, nullable=True)
         longitude = Column(Float, nullable=True)
         reviews = relationship('Review', backref='place')
+        amenities = relationship('Amenity', secondary='place_amenity',
+                                 viewonly=False, backref="place_amenties")
     else:
         city_id = ""
         user_id = ""
@@ -42,3 +54,12 @@ class Place(BaseModel, Base):
                 if review.place_id == self.id:
                     reviews_list.append(review)
             return reviews_list
+
+        @property
+        def amenities(self):
+            amenity_list = []
+            from models.amenity import Amenity
+            for amenity in models.storage.all(Amenity).values:
+                if amenity.place_id == self.id:
+                    amenity_list.append(amenity)
+            return amenity_list
