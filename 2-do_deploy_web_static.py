fe9eb0a67d37e2ce61b2088402174608717da7c5
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-04-07 01:49:18 +0200

    fix pycodestyle

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -22,13 +22,13 @@ def do_deploy(archive_path):
         file_name = fn.split(".")[0]
         path = "/data/web_static/releases/"
         run("mkdir -p {}{}/".format(path, file_name))
-        run("tar -xzf /tmp/{} -C {}{}".format(\
-        archive_path, path, file_name))
+        run("tar -xzf /tmp/{} -C {}{}".format(
+            archive_path, path, file_name))
         run("rm /tmp/{}".format(fn))
         run("mv {0}{1}/web_static/* {0}{1}/".format(path, file_name))
         run("rm -rf {}{}/web_static/".format(path, file_name))
         run("rm -rf /data/web_static/current")
         run("ln -s {}{}/ /data/web_static/current".format(path, file_name))
         return True
-    except:
+    except Exception as e:
         return False
