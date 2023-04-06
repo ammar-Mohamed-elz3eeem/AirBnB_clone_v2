c9e86711e3833eaa4044a5f41e5be6c54123485c
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-04-06 23:55:49 +0200

    remove install function

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,17 +9,8 @@
 # - make /data/web_static/current symbolic link to 
 # 	/data/web_static/releases/test/
 
-function install() {
-	command -v "$1" > /dev/null;
-
-	if [[ $EXITCODE -ne 0 ]]; then
-		sudo apt-get update -y -qq;
-		sudo apt-get install "$1" -y -qq;
-	fi;
-}
-
-install nginx
-
+sudo apt-get update -y -qq;
+sudo apt-get install "$1" -y -qq;
 sudo ufw allow 'Nginx HTTP'
 
 sudo mkdir -p /data/web_static/releases/test/
