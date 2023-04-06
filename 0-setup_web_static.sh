8c57d4450d0ee328935370915cd57b1948841259
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-04-07 00:41:49 +0200

    fix rendering of html

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -19,15 +19,13 @@ sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-sudo echo "
-<html>
+sudo echo "<html>
   <head>
   </head>
   <body>
     Holberton School
   </body>
-</html>
-" | sudo tee /data/web_static/releases/test/index.html
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
