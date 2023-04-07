342fe53f741cd4909d5a3068bbe6fa29c2cd6f0d
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-04-08 01:21:19 +0200

    add configs for root folder of server

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -59,6 +59,24 @@ exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
 
+file { '/var/www':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html':
+  ensure => 'directory'
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
 file { '/etc/nginx/sites-enabled/default':
   ensure  => 'present',
   content => $nginx_conf
