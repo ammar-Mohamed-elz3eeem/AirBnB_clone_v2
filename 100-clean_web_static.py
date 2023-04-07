6a7ec51f1489b36f6eca50f5836786cb647a8d74
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-04-07 15:44:22 +0200

    fix os lib & rewrite do_clean docstring

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,18 +3,27 @@
 This script will deploy all my work to remote servers i have
 using fabric commands
 """
+
 from fabric.api import *
-from os import listdir, path as p
+import os
 
 env.hosts = ["100.25.200.200", "54.237.27.107"]
 
 
 def do_clean(number=0):
-    """ clean all releases from my web servers """
+    """
+    clean all releases from my web servers according to number
+    Args:
+        number (int): number of releases we want to keep
+        in the system, if number less than or equal to 1 so
+        system will kep only most recent release
+    """
+
     number = int(number)
     number = 1 if number <= 0 else number
-    tars = sorted(listdir("./versions"))
+    tars = sorted(os.listdir("./versions"))
     length = len(tars) - number
+
     with lcd("versions"):
         [local(f"rm ./{tars[i]}") for i in range(length)]
 
