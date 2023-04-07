7b753ad8f7af33708db51e91668bacce4788d320
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-04-08 00:40:54 +0200

    require on all script but last one

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -3,33 +3,33 @@
 package { 'nginx':
   ensure   => installed,
   provider => 'apt'
-}
+} ->
 
 file { '/data':
   ensure => 'directory',
   group  => 'ubuntu',
   owner  => 'ubuntu'
-}
+} ->
 
 file { '/data/web_static':
   ensure => 'directory'
-}
+} ->
 
 file { '/data/web_static/releases':
   ensure => 'directory'
-}
+} ->
 
 file { '/data/web_static/shared':
   ensure => 'directory'
-}
+} ->
 
 file { '/data/web_static/releases/test':
   ensure => 'directory'
-}
+} ->
 
 file { '/data/web_static/current':
   ensure => '/data/web_static/releases/test'
-}
+} ->
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
@@ -40,13 +40,13 @@ file { '/data/web_static/releases/test/index.html':
     Holberton School
   </body>
 </html>"
-}
+} ->
 
 exec { 'configure nginx':
   command  => 'sed -i "/listen 80 default_server/a location /hbnb_static {alias /data/web_static/current/;}" /etc/nginx/sites-enabled/default',
   provider => 'shell',
   require  => Package['nginx']
-}
+} ->
 
 service { 'nginx':
   ensure  => 'running',
