72d8ab2ece5321dc283c56a20ea09f347cc91c1f
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-04-21 22:08:36 +0200

    add /c/<string> route

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Flask web framework module used for routing /
+path with content set to Hello HBNB!"""
+from flask import Flask
+app = Flask(__name__, )
+
+
+@app.route('/', strict_slashes=False)
+def hello_world():
+    """function to handle requests made to / route
+
+    Returns:
+        string: content of / route
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_func():
+    """function to handle requests made to /hbnb route
+
+    Returns:
+        string: content of /hbnb route
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_func(text):
+    """function to handle requests made to /c/<text> route
+
+    Returns:
+        string: c <text>
+    """
+    return "C " + text.replace("_", " ")
+
+if __name__ == "__main__":
+    app.run('0.0.0.0')
