4ad36d1ef73dda0eb37501072ebe6a8ff381a158
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-04-21 19:21:47 +0200

    add documentation for routes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,8 +1,21 @@
+#!/usr/bin/env python3
+""" Flask web framework module used for routing /
+path with content set to Hello HBNB!
+"""
+
+
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
+    """ function to handle requests made to / route
+
+    Returns:
+        string: content of / route
+    """
     return 'Hello HBNB!'
+
 
 app.run("0.0.0.0")
