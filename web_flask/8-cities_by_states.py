e99d72f7776dc2414b4690598f50d9eea44a8934
Ammar-Mohamed-Elz3eeem <ammarmohamedthez@gmail.com>
2023-04-22 08:09:27 +0200

    add /cities_by_states route with template

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" module for getting all states in our system
+alongside with cities found in that state"""
+from flask import Flask, render_template
+from models import storage
+from os import getenv
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(e):
+    if storage is not None:
+        storage.close()
+
+
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    states = storage.all("State").values()
+    return render_template("8-cities_by_states.html", states=states)    
+
+
+if __name__ == "__main__":
+    app.run('0.0.0.0', 5000)
